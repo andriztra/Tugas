d3149d1636aa1fc420e074e99663cff71f3ecf7b
Andeno Rizkyan Putra <143734212+andriztra@users.noreply.github.com>
2023-09-24 10:41:30 +0700

    24 sept 2023 forum.c

---
+++ 24 sept 2023 forum.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main() {
+    char nama1[20]; // Mendeklarasikan array karakter dengan kapasitas 20
+    char *nama2;    // Mendeklarasikan pointer karakter
+    char *nama3[20]; // Mendeklarasikan array 20 pointer karakter
+
+    // Mengisi nama1 dengan string "Brisia Jodie"
+    strcpy(nama1, "Brisia Jodie");
+
+    // Mengalokasikan memori untuk nama2 dan menyalin string "Brisia Jodie" ke dalamnya
+    nama2 = (char *)malloc(strlen("Brisia Jodie") + 1);
+    strcpy(nama2, "Brisia Jodie");
+
+    // Mengalokasikan memori untuk nama3[0] dan menyalin string "Brisia Jodie" ke dalamnya
+    nama3[0] = (char *)malloc(strlen("Brisia Jodie") + 1);
+    strcpy(nama3[0], "Brisia Jodie");
+
+    // Cetak hasilnya
+    printf("nama1: %s\n", nama1);
+    printf("nama2: %s\n", nama2);
+    printf("nama3[0]: %s\n", nama3[0]);
+
+    // Jangan lupa untuk membebaskan memori yang dialokasikan
+    free(nama2);
+    free(nama3[0]);
+
+    return 0;
+}
